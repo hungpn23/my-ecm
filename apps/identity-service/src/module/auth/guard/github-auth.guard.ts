7e65ccf22803a358b8343d743134ee97bfc71ee3
hungpn23 <hungpn23@gmail.com>
2026-07-15 18:34:39 +0700

    refactor(auth): simplify GithubAuthGuard and GoogleAuthGuard by removing unnecessary constructor

--- apps/identity-service/src/module/auth/guard/github-auth.guard.ts
+++ apps/identity-service/src/module/auth/guard/github-auth.guard.ts
@@ -3,8 +3,4 @@ import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class GithubAuthGuard extends AuthGuard(PASSPORT_STRATEGY.GITHUB) {
-  constructor() {
-    super();
-  }
-}
+export class GithubAuthGuard extends AuthGuard(PASSPORT_STRATEGY.GITHUB) {}
--- apps/identity-service/src/module/auth/guard/google-auth.guard.ts
+++ apps/identity-service/src/module/auth/guard/google-auth.guard.ts
@@ -3,8 +3,4 @@ import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class GoogleAuthGuard extends AuthGuard(PASSPORT_STRATEGY.GOOGLE) {
-  constructor() {
-    super();
-  }
-}
+export class GoogleAuthGuard extends AuthGuard(PASSPORT_STRATEGY.GOOGLE) {}
