312d76b63500561c6ca5ff8069d956eccf755698
hungpn23 <hungpn23@gmail.com>
2026-08-31 14:06:31 +0700

    fix: incorrect comment in decorators

--- apps/identity-service/src/module/oauth/github/github.decorator.ts
+++ apps/identity-service/src/module/oauth/github/github.decorator.ts
@@ -5,7 +5,7 @@ import type { GithubAuthenticatedRequest } from "./github.type";
 export const GithubProfile = createParamDecorator(
   (data: keyof Profile, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest<GithubAuthenticatedRequest>();
-    const user = request.user; // user is set in the JwtStrategy.validate() method after successful authentication
+    const user = request.user; // user is set in the GithubStrategy.validate() method after successful authentication
 
     return data ? user[data] : user;
   },
--- apps/identity-service/src/module/oauth/google/google.decorator.ts
+++ apps/identity-service/src/module/oauth/google/google.decorator.ts
@@ -5,7 +5,7 @@ import type { GoogleAuthenticatedRequest } from "./google.type";
 export const GoogleProfile = createParamDecorator(
   (data: keyof Profile, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest<GoogleAuthenticatedRequest>();
-    const user = request.user; // user is set in the JwtStrategy.validate() method after successful authentication
+    const user = request.user; // user is set in the GoogleStrategy.validate() method after successful authentication
 
     return data ? user[data] : user;
   },
