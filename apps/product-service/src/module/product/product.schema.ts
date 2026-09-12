7de50e3fc1c777be3f2fd5290566bc3cfdbe8d1e
hungpn23 <hungpn23@gmail.com>
2026-09-13 04:10:20 +0700

    refactor(schema): update response types to use inferIn

--- apps/product-service/src/module/product/product.schema.ts
+++ apps/product-service/src/module/product/product.schema.ts
@@ -1,4 +1,4 @@
-// import { Uuid } from "@libs/common";
+import { EntityResponse, NonNegativeDecimal, Paginated, Uuid, Varchar } from "@libs/common";
 import { type } from "arktype";
 
 export const PRODUCT_STATUS = {
@@ -8,39 +8,23 @@ export const PRODUCT_STATUS = {
 export const ProductStatus = type.enumerated(...Object.values(PRODUCT_STATUS));
 export type ProductStatus = typeof ProductStatus.infer;
 
-// export const CreateProduct = type({
-//   name: "string <= 255",
-//   "description?": "string <= 255",
-//   sku: "string.alphanumeric <= 10",
-//   price: "number >= 0",
-//   "status?": ProductStatus,
-//   categoryId: Uuid,
-// });
-// export type CreateProduct = typeof CreateProduct.infer;
+export const CreateProduct = type({
+  name: Varchar,
+  "description?": "string >= 1",
+  price: NonNegativeDecimal,
+  status: ProductStatus.default(PRODUCT_STATUS.ACTIVE),
+  categoryId: Uuid,
+});
+export type CreateProduct = typeof CreateProduct.infer;
 
-// export const ProductResponse = type({
-//   id: "string.uuid",
-//   createdAt: "string",
-//   updatedAt: "string",
-//   categoryId: "string.uuid",
-//   name: "string",
-//   description: "string | null",
-//   sku: "string",
-//   price: "string.numeric.parse",
-//   status: ProductStatus,
-// });
-// export type ProductResponse = typeof ProductResponse.infer;
+export const ProductResponse = EntityResponse.merge({
+  name: Varchar,
+  description: "string | null",
+  price: NonNegativeDecimal,
+  status: ProductStatus,
+  categoryId: Uuid,
+});
+export type ProductResponse = typeof ProductResponse.inferIn;
 
-// export const ProductListResponse = ProductResponse.array();
-
-// export const Test = type({
-//   status: "string",
-//   name: "string",
-//   randomType: "string",
-//   price: "string.numeric.parse",
-//   description: "string",
-//   id: "string.uuid",
-//   createdAt: "string",
-//   updatedAt: "string",
-//   category: "string",
-// });
+export const PaginatedProductResponse = Paginated(ProductResponse);
+export type PaginatedProductResponse = typeof PaginatedProductResponse.inferIn;
