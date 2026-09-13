e07aee607d5d58f96d145b5d9c8009bebc8f9a8d
hungpn23 <hungpn23@gmail.com>
2026-09-13 13:16:32 +0700

    feat(product-service): add category endpoints and product scaffold

--- apps/product-service/src/module/product/product.controller.ts
+++ apps/product-service/src/module/product/product.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Endpoint } from "@libs/common";
+import { Body, Controller, NotImplementedException } from "@nestjs/common";
+import { CreateProduct, ProductResponse } from "./product.schema";
 import { ProductService } from "./product.service";
 
 @Controller("products")
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Endpoint({
+    method: "POST",
+    request: CreateProduct,
+    response: ProductResponse,
+  })
+  async create(@Body({ schema: CreateProduct }) _body: CreateProduct): Promise<ProductResponse> {
+    throw new NotImplementedException();
+  }
 }
