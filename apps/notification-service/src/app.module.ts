20907a432f7846154b0d1350d9ec64565c70d85d
hungpn23 <hungpn23@gmail.com>
2026-09-21 15:16:50 +0700

    feat: onboard notification service

--- apps/notification-service/src/app.module.ts
+++ apps/notification-service/src/app.module.ts
@@ -2,28 +2,21 @@ import { ArktypeValidationPipe } from "@libs/common";
 import {
   AuthModule,
   ConfigModule,
-  DatabaseModule,
   JwtGuard,
   KafkaModule,
   LoggerModule,
   RedisModule,
 } from "@libs/core";
-import { entities } from "@mikro-orm/generated";
 import { Module, StandardSchemaSerializerInterceptor } from "@nestjs/common";
 import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from "@nestjs/core";
-import { CategoryModule } from "./module/category/category.module";
-import { ProductModule } from "./module/product/product.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    DatabaseModule.forRoot(entities),
     LoggerModule.forRoot(),
     RedisModule.forRoot(),
     KafkaModule.forRoot(),
     AuthModule,
-    CategoryModule,
-    ProductModule,
   ],
   providers: [
     {
---
+++ apps/notification-service/src/main.ts
@@ -0,0 +1,42 @@
+import { appConfig, KafkaOptionsFactory } from "@libs/core";
+import { NestFactory } from "@nestjs/core";
+import { type KafkaOptions } from "@nestjs/microservices";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger, registerMicroserviceLogging } from "nestjs-pino";
+import "reflect-metadata";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    routeConflictPolicy: { duplicate: "error", shadow: "error" },
+  });
+
+  app.enableShutdownHooks();
+
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+
+  const { APP_HOST, APP_PORT } = app.get(appConfig.KEY);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Notification Service API")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("swagger", app, documentFactory);
+
+  const ms = app.connectMicroservice<KafkaOptions>(
+    app.get(KafkaOptionsFactory).createClientOptions(),
+    { deferInitialization: true },
+  );
+  registerMicroserviceLogging(ms);
+
+  await app.startAllMicroservices();
+  await app.listen(APP_PORT, APP_HOST);
+
+  logger.log(`🔥 Swagger: http://${APP_HOST}:${APP_PORT}/swagger`);
+}
+
+await bootstrap();
